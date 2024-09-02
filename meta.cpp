50478e2d304f98b30dd2559d5d44ba0c87aaad59
Jonathan Merrill <99621514+UbiquitousNull@users.noreply.github.com>
2024-09-02 13:23:51 +1200

    added files

---
+++ meta.cpp
@@ -0,0 +1,6 @@
+name = "My Mod";
+picture = "path_to_mod_logo.paa";
+action = "https://mymodwebsite.com";
+author = "My Name";
+version = "1.0";
+description = "This is a Dayz crypto mining mod that allows.";
---
+++ mod.cpp
@@ -0,0 +1,6 @@
+name = "Dayz Crypto";
+dir = "@DayzCrypto";
+author = "UbiquitousNull";
+tooltip = "A custom crypto mining mod for DayZ";
+hidePicture = 0;
+hideName = 0;
