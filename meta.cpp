f7686907f6a7f24a3d41d756846207b6f74484f4
Jonathan Merrill <99621514+UbiquitousNull@users.noreply.github.com>
2024-09-04 09:10:14 +1200

    001

--- meta.cpp
+++ meta.cpp
@@ -3,4 +3,4 @@ picture = "path_to_mod_logo.paa";
 action = "https://mymodwebsite.com";
 author = "My Name";
 version = "1.0";
-description = "This is a Dayz crypto mining mod that allows.";
+description = "This is a Dayz crypto mining mod.";
